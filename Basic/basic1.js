d2c723973be10465248887a9338cdf5730c05638
Aashiqsingh <aashiqsingh56789@gmail.com>
2024-09-22 16:41:33 +0530

    data type

--- Basic/basic1.js
+++ Basic/basic1.js
@@ -1,4 +1,38 @@
-console.log("Hello world!");
-console.info("this is a information function");
-console.warn("please give my money....");
-console.error("404 page not found...")
+// console.log("Hello world!");
+// console.info("this is a information function");
+// console.warn("please give my money....");
+// console.error("404 page not found...")
+
+
+// Data type : number , string , undefined , boolean , null
+
+a = 10;
+console.log("value of a = ",a);
+console.log("type of a = ",typeof(a));
+
+b = "12.56";
+console.log("value of b = ",b);
+console.log("type of b = ",typeof(b));
+
+c = "chhands";
+console.log("value of c = ",c);
+console.log("type of c = ",typeof c);
+
+isAlive = true;
+console.log("isAlive = ",isAlive);
+console.log("type of isAlive = ",typeof isAlive);
+
+var x;
+console.log("value of x = ",x);
+console.log("type of x = ",typeof x);
+
+z = null;
+console.log("value of z = ",z);
+console.log("type of z = ",typeof z);
+
+
+
+
+
+
+
