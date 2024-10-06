e7a4dd374f8879e8d73f08cd3e5b5ea06e07dacb
Aashiqsingh <aashiqsingh56789@gmail.com>
2024-10-06 16:35:59 +0530

    operator

---
+++ Operator/AirthmaticOpt.js
@@ -0,0 +1,30 @@
+var a = 4;
+var b = 3;
+
+
+// Airrthmatic operator
+
+// console.log("Addition = ",a+b);
+
+// console.log("Subtraction = ",a-b);
+
+// console.log("Multiplication = ",a*b);
+
+// console.log("Division = ",a/b);
+// console.log("module = ",a%b);
+
+// console.log("Exponent = ",b**a);
+
+// 3*3*3*3 
+
+
+// Assignment operator 
+// console.log(a);
+
+// // a = a + 1;
+
+// a += 1;
+// console.log(a);
+
+// fact = fact * i;
+// fact *= i;
---
+++ Operator/Relational.js
@@ -0,0 +1,21 @@
+var a = '10';
+var b = '10'; // string
+
+
+console.log(a == b);
+// console.log(a != b);
+
+// console.log(a < b);
+// console.log(a > b);
+
+// console.log(a <= b);
+
+// console.log(a >= b);
+
+// Strick check 
+
+console.log(a === b);
+console.log(a !== b);
+
+
+
