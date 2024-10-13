744f283fa00d57f83b886fc2be2b0016b528d0e6
Aashiqsingh <aashiqsingh56789@gmail.com>
2024-10-13 14:17:10 +0530

    input

---
+++ Input/Input1.js
@@ -0,0 +1,15 @@
+// alert("Welcome to learing js") /// output
+
+// var x = confirm("Are you married?") // true or false
+
+// console.log(x);
+
+var a = prompt("Enter the name :")
+var b = 
+
+console.log(x);
+
+console.log(typeof x);
+
+
+
