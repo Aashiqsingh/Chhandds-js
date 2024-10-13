5d19e434d6499ea174c3a1060e5b14afa46f59d9
Aashiqsingh <aashiqsingh56789@gmail.com>
2024-10-13 14:56:20 +0530

    input

--- Input/Input1.js
+++ Input/Input1.js
@@ -4,12 +4,42 @@
 
 // console.log(x);
 
-var a = prompt("Enter the name :")
-var b = 
+// var a = prompt("Enter the name :")
+// var b = 
 
-console.log(x);
+// console.log(x);
 
-console.log(typeof x);
+// console.log(typeof x);
 
 
+// var a = 12
+
+// var b = 12
+
+
+// console.log(a + b);
+
+
+// var a = parseInt(prompt("Enter first number :"));
+
+// var b = parseInt(prompt("Enter second number :"));
+
+// // var c = parseInt(a) + parseInt(b);
+
+// console.log("Addition = ",a+b);
+
+
+
+var eng = 90;
+var math = 90;
+var sci = 90;
+
+
+var total = math+eng+sci;
+
+console.log("Addition = ",total);
+
+// var per = total/300 * 100;
+var per = total/3;
+console.log(per);
 
