e46dc435f960369b86696d76d9b7f3053631827d
Aashiqsingh <aashiqsingh56789@gmail.com>
2024-11-10 16:26:29 +0530

    ifElse

---
+++ ifElse/ifElse1.js
@@ -0,0 +1,76 @@
+// var num = -4;
+
+// if(num > 0){
+//     console.log("Positive number");
+    
+// }
+// else{
+//     console.log("Negative number");
+// }
+
+// var num = parseInt(prompt("Enter the number :"))
+
+// if(num % 2 == 0){
+//     console.log("Even number");
+// }
+// else{
+//     console.log("Odd number");
+// }
+
+
+// var a = 22;
+// var b = 10;
+// var c = 19;
+
+// if(a > b){
+//     if(a > c){
+//         console.log(a," is bigger");
+        
+//     }
+//     else{
+//         console.log(c," is bigger");
+//     }
+// }
+// else{
+//     if(b > c){
+//         console.log(b," is bigger");
+//     }
+//     else{
+//         console.log(c," is bigger");
+//     }
+// }
+
+
+var isAlive = false;
+var age = 17;
+if(isAlive){
+    console.log("you are alive");
+    if(age > 18){
+        console.log("You are an adult");
+    }
+    else{
+        console.log("You are a child");
+    }
+
+}
+else{
+        console.log("You are not alive");
+
+}
+
+
+
+
+// if(isAlive == true){
+//     console.log("you are alive");
+//     if(age > 18){
+//         console.log("You are an adult");
+//     }
+//     else{
+//         console.log("You are a child");
+//     }
+    
+// }
+// else{
+//     console.log("You are not alive");
+// }
