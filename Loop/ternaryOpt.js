c5c86a39afe375c1a6a20dd0492b785fbcccb952
Aashiqsingh <aashiqsingh56789@gmail.com>
2024-11-17 16:56:35 +0530

    ternary

--- Loop/ternaryOpt.js
+++ Loop/ternaryOpt.js
@@ -1,5 +1,5 @@
-var a = 39;
-var b = 22;
+var a = 59;
+var b = 42;
 
 // if(a > b){
 //     console.log("A is bigger");
@@ -11,4 +11,12 @@ var b = 22;
 
 // var x = a > b ? "A is gretter" : "B is gretter";
 // console.log(x);
+
+
+// a > b ? console.log("A is bigger") : console.log("B is bigger");
+
+
+// var x = a > b ? a : b;
+// console.log(x + " is bigger");
+
 
