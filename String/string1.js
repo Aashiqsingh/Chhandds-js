a6befb6a3f58cbfe45fd4767e1e55cb8eba207ec
Aashiqsingh <aashiqsingh56789@gmail.com>
2024-12-01 14:40:01 +0530

    string

---
+++ String/string1.js
@@ -0,0 +1,83 @@
+var str = "Royal technosoft pvt ltd";
+var str2 = "chhands vyas"
+
+// var x = str.toLowerCase()
+var x = str.toUpperCase()
+
+console.log(x);
+
+
+
+// var x = str.split("a")
+// var x = str.split(" ")
+// var x = str.split("")
+
+
+// console.log(x);
+
+
+
+// var x = str.slice(1)
+// var x = str.slice(1,4)
+// var x = str.slice(6,13)
+// var x = str.slice(-1)
+// var x = str.slice(-3,-1)
+
+
+
+// console.log(x);
+
+
+
+
+// var x = str.replaceAll("o","z")
+// console.log(x);
+
+
+// var x = str.replace("o","x")
+// var x = str.replace("oyal","z")
+// var x = str.replace("o","zoyal")
+
+// console.log(x);
+
+
+// var x = str.repeat(1)
+// console.log(x);
+
+
+
+
+// var x = str.indexOf("t")
+// console.log(x);
+
+// var x = str.includes("soft")
+// console.log(x);
+
+
+
+
+
+// console.log(str);
+// console.log(str.length);
+
+// var x = str.charAt(10)
+// console.log(x);
+
+// var x = str.charCodeAt(0)
+// console.log(x);
+
+
+// var x = str.concat(str2)
+// var x = str2.concat(str)
+// console.log(x);
+
+// var x = str + str2;
+// console.log(x);
+
+// var x = str.endsWith(" ")
+// var x = str.endsWith("ltd")
+
+// console.log(x);
+
+// var x = str.startsWith("Royal")
+// console.log(x);
