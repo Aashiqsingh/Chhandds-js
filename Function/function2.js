b1c2fdf8401ec60980632330590e557735115a83
Aashiqsingh <aashiqsingh56789@gmail.com>
2024-12-08 16:49:36 +0530

    function

---
+++ Function/function2.js
@@ -0,0 +1,67 @@
+function add(a,b)
+{
+    console.log("Addition = ",a+b);
+    
+}
+
+function subtract(a,b)
+{
+    console.log("Subtraction = ",a-b);
+    
+}
+
+function multiply(a,b){
+    console.log("Multiplication = ",a*b);
+
+}
+
+function divide(a,b){
+    console.log("Division = ",a/b);
+    
+}
+
+
+var choice = parseInt(prompt("1 for add\n2 for sub\n3 for mul\n4 for div \n\n\n Enter your choice"))
+
+var a = 18;
+var b = 4;
+
+
+// switch(choice){
+
+//     case 1:
+//         add(a,b);
+//         break;
+//     case 2:
+//         subtract(a,b);
+//         break;
+//     case 3:
+//         multiply(a,b);
+//         break;
+//     case 4:
+//         divide(a,b);
+//         break;
+    
+// }
+
+
+// task  : no return type with arguments
+
+// function : find prime number
+// function : find reverse  number
+// function find even odd 
+
+
+// function isEvenOdd(x)
+// {
+//     if(x % 2 == 0)
+//     {
+//         console.log("Even number");
+        
+//     }
+//     else{
+//         console.log("Odd number");
+//     }
+// }
+
+// isEvenOdd(19)
