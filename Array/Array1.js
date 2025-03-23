e024f13d959385e68ad3f817905f2cd1211dbbf3
Aashiqsingh <aashiqsingh56789@gmail.com>
2025-03-23 16:38:55 +0530

    push , pop

---
+++ Array/Array1.js
@@ -0,0 +1,65 @@
+// var arr = ["ram",63 ,7635.2 , true]
+
+var users = ["harsh","rajvi","moksh","lakshya","hiral"];
+
+
+// var x = users.reverse()
+// console.log(x);
+
+
+// var x = users.sort()
+// console.log(x);
+
+
+
+
+
+// var x = users.push("apsara")
+// console.log(x);
+// users.push("apsara","rahul","tejas")
+
+// console.log(users);
+
+
+// users.pop()
+
+
+// console.log(users);
+
+// users.unshift("rahul","tejas")
+
+// console.log(users);
+
+// var x = users.shift()
+// console.log(x);
+
+
+// console.log(users);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log(users);
+// console.log(users.length);
+
+// console.log(users[1]);
+// console.log(users[2]);
+
+// for(let i=0;i<users.length;i++){
+
+//     console.log(users[i].toUpperCase());
+    
+// }
+
+
+
