13013b896e83e0ba8cc6401f6efce7af86827465
Aashiqsingh <aashiqsingh56789@gmail.com>
2025-04-06 16:17:23 +0530

    dom

---
+++ Dom/dom1.js
@@ -0,0 +1,11 @@
+// DOM --> DOcument object model 
+
+function changeTxt(){
+    let txt = document.getElementById("txt")
+
+    txt.innerText = "Royal technosoft pvt ltd."
+    txt.style.backgroundColor = "red"
+    txt.style.color = "white"
+    txt.style.height = "200px";
+    txt.style.width = "200px";
+}
