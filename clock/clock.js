03efeed8b120b77b2d5300d9e8b4269492449f35
Aashiqsingh <aashiqsingh56789@gmail.com>
2025-07-20 16:26:40 +0530

    clock

---
+++ clock/clock.js
@@ -0,0 +1,23 @@
+const getTime = ()=>{
+
+    let date = new Date();
+
+    let hour = date.getHours();
+    let min = date.getMinutes();
+    let sec = date.getSeconds();
+
+
+    let hours = document.getElementById("hours");
+    let mins = document.getElementById("mins");
+    let secs = document.getElementById("secs");
+
+
+    hours.innerHTML = hour < 10 ? "0" + hour : hour;
+    mins.innerHTML = min < 10 ? "0" + min : min;
+    secs.innerHTML = sec < 10 ? "0" + sec : sec;
+}
+
+
+setInterval(()=>{
+    getTime();
+},1000);
